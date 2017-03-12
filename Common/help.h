8d1db23b3a156c4dff23e2162f2e976ae06e12a5
Coding <coding@macbook-pro.local>
2017-03-12 11:19:08 +0800

    细节修改；

--- Common/help.h
+++ Common/help.h
@@ -12,12 +12,15 @@
 #if TARGET_OS_IPHONE
 
 @import UIKit;
+
 #define RootView UIViewController
 #define NameView(name) UI##name
 #define CheckButton UISwitch
 #define ProgressView UIProgressView
 #define IMAGE_CLASS UIImage
+
 #else
+
 @import Cocoa;
 #define RootView NSViewController
 #define NameView(name) NS##name
